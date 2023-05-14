32d798b91439b98adc4775377538a62aa9967fce
Son Le <sonlele2000@gmail.com>
2023-05-14 20:42:04 +0700

    first commit

--- js/test.js
+++ js/test.js
@@ -1,25 +1,13 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+document.getElementById('btn-modal').addEventListener('click', function () {
+  document.getElementById('overlay').classList.add('is-visible');
+  document.getElementById('modal').classList.add('is-visible');
+});
 
-// Get the button that opens the modal
-var btn = document.getElementById('myBtn');
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName('close')[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function () {
-  modal.style.display = 'block';
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = 'none';
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-};
+document.getElementById('close-btn').addEventListener('click', function () {
+  document.getElementById('overlay').classList.remove('is-visible');
+  document.getElementById('modal').classList.remove('is-visible');
+});
+document.getElementById('overlay').addEventListener('click', function () {
+  document.getElementById('overlay').classList.remove('is-visible');
+  document.getElementById('modal').classList.remove('is-visible');
+});
